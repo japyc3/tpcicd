8bcef904473035fdfd8f240f8424ca16ea6adfcb
Emmanuel MINDONGO <mdg.m.emmanuel@gmail.com>
2023-12-09 00:24:48 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {title = 'emmaPortFolio';
+name = 'Mindongo Emma';
+classe = 'Inge 4 ISI';
+role = 'Backend Développeur';
+}
