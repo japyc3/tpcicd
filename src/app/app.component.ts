7b3907c71172798921be9c343660edc90d95f67d
Nsisse Prosper <prosper.nsisse@institutsaintjean.org>
2023-12-11 14:18:43 +0100

    Test pipeline de Vercel

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+title = 'TP CI/CD';
+name = 'NSISSE Prosper';
+classe = 'ING 4 ISI';
+message = 'Bonjour le monde !!';
+}
